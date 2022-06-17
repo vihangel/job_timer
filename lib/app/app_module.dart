a09f58289a7426722d4e2a3aa0397a38a280ab96
Angel <47459464+vihangel@users.noreply.github.com>
2022-06-17 19:36:53 -0300

    feat auth google - not completed

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/modules/login/login_module.dart';
+import 'package:job_timer/app/modules/services/auth/auth_service.dart';
+import 'package:job_timer/app/modules/services/auth/auth_service_impl.dart';
 
 import 'modules/home/home_module.dart';
 import 'modules/splash/splash_page.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind.lazySingleton<AuthService>((i) => AuthServiceImpl()),
+      ];
 
   @override
   List<ModularRoute> get routes => [
